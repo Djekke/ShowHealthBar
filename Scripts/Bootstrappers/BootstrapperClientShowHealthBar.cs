a5c7fe7fc5e57aa93c58fa1881d2c87a56063bc1
Djekke <djekke@gmail.com>
2019-04-12 15:42:30 +0300

    Init commit. 0.1.0 release.

---
+++ Scripts/Bootstrappers/BootstrapperClientShowHealthBar.cs
@@ -0,0 +1,32 @@
+﻿namespace CryoFall.CNEI.Bootstrappers
+{
+    using AtomicTorch.CBND.CoreMod.Bootstrappers;
+    using AtomicTorch.CBND.CoreMod.Characters;
+    using AtomicTorch.CBND.CoreMod.UI.Controls.Game.WorldObjects.Bars;
+    using AtomicTorch.CBND.GameApi.Data.Characters;
+    using AtomicTorch.CBND.GameApi.Scripting;
+
+    public class BootstrapperClientShowHealthBar: BaseBootstrapper
+    {
+        public override void ClientInitialize()
+        {
+            BootstrapperClientGame.InitEndCallback += GameInitHandler;
+        }
+
+        private static void GameInitHandler(ICharacter currentCharacter)
+        {
+            var clientState = currentCharacter.GetClientState<PlayerCharacterClientState>();
+            var publicState = currentCharacter.GetPublicState<PlayerCharacterPublicState>();
+
+            clientState.HealthbarControl = Client.UI.AttachControl(
+                currentCharacter,
+                new CharacterHealthBarControl()
+                    { CharacterCurrentStats = publicState.CurrentStats },
+                positionOffset: (
+                    0,
+                    currentCharacter.ProtoCharacter
+                        .CharacterWorldHeight),
+                isFocusable: false);
+        }
+    }
+}
