16f725c14d29beb89076e983f24ae1a108240e0a
djekke <djekke@gmail.com>
2020-04-17 19:47:29 +0300

    Fix A26 incompatibility. Simplification.

--- Scripts/Bootstrappers/BootstrapperClientShowHealthBar.cs
+++ Scripts/Bootstrappers/BootstrapperClientShowHealthBar.cs
@@ -1,7 +1,6 @@
 ﻿namespace CryoFall.ShowHealthBar.Bootstrappers
 {
     using AtomicTorch.CBND.CoreMod.Bootstrappers;
-    using AtomicTorch.CBND.CoreMod.Characters;
     using AtomicTorch.CBND.CoreMod.UI.Controls.Game.WorldObjects.Character;
     using AtomicTorch.CBND.GameApi.Data.Characters;
     using AtomicTorch.CBND.GameApi.Scripting;
@@ -15,9 +14,7 @@
 
         private static void GameInitHandler(ICharacter currentCharacter)
         {
-            var clientState = currentCharacter.GetClientState<PlayerCharacterClientState>();
-
-            clientState.HealthbarControl = Client.UI.AttachControl(
+            Client.UI.AttachControl(
                 currentCharacter,
                 new CharacterOverlayControl(currentCharacter),
                 positionOffset: (0, currentCharacter.ProtoCharacter.CharacterWorldHeight),
